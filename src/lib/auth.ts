d76b00b94fbc2acc3fdd49bb433982e983963664
“Anmol” <pal592anmol@gmail.com>
2026-07-14 16:43:42 +0530

    ADD : GitHub OAuth

---
+++ src/lib/auth.ts
@@ -0,0 +1,20 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import prisma from './db'
+
+
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "postgresql",
+    }),
+    baseURL: "http://localhost:3005",
+    basePath: "/api/auth",
+    trustedOrigins: ["http://localhost:3000"],
+    socialProviders: { 
+    github: { 
+      clientId: process.env.GITHUB_CLIENT_ID as string, 
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string, 
+    }, 
+  },
+});
